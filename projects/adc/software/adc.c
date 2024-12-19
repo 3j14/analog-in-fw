2f8856a341eb7d27035c22b515445a4d8e3b58e0
Jonas Drotleff <jonas.drotleff@3j14.de>
2024-12-19 19:46:57 +0100

    ADC: WIP on user space support

--- projects/adc/software/adc.c
+++ projects/adc/software/adc.c
@@ -1,4 +1,49 @@
-#include "dmadc.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
 
-int main(int argc, char *argv[]) { return 0; }
+#include "adc.h"
+#include "dmadc.h"
+
+void reg_write(struct adc_config *adc, uint32_t data) {
+    *(adc->adc_reg) = data;
+}
+
+struct dma_channel {
+    struct channel_buffer *buffer;
+    int fd;
+};
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s num_transfers", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    struct dma_channel channel;
+    channel.fd = open("/dev/dmadc", O_RDWR);
+    if (channel.fd == -1) {
+        printf("Unable to open '/dev/dmadc'. Is the driver loaded?");
+        exit(errno);
+    }
+    // Map the buffer from kernel to user space
+    channel.buffer = (struct channel_buffer *)mmap(
+        NULL,
+        sizeof(struct channel_buffer),
+        PROT_READ,
+        MAP_SHARED,
+        channel.fd,
+        0
+    );
+    if (channel.buffer == MAP_FAILED) {
+        printf("Unable to map memory from kernel");
+        exit(errno);
+    }
+
+    printf("Start transfer");
+    ioctl(channel.fd, START_XFER);
+}
