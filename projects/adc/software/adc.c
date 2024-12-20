13386557d42fc37935edf534c58d79fbc0aec2fd
Jonas Drotleff <jonas.drotleff@3j14.de>
2024-12-20 17:38:59 +0100

    ADC: Configure and read

--- projects/adc/software/adc.c
+++ projects/adc/software/adc.c
@@ -15,15 +15,15 @@ int main(int argc, char *argv[]) {
     if (rc < 0) {
         exit(-rc);
     }
+    channel.buffer->length = BUFFER_SIZE;
+    channel.buffer->period_len = 10 * sizeof(uint32_t);
 
     rc = open_adc(&adc);
     if (rc < 0) {
         exit(-rc);
     }
 
-    // Enable power and IO
-    *adc.config.config =
-        ADC_PWR_EN | ~ADC_REF_EN | ADC_IO_EN | ~ADC_DIFFAMP_EN | ~ADC_OPAMP_EN;
+    *adc.config.config = ADC_PWR_EN | ADC_IO_EN;
     // Wait one second for power to stabilize
     sleep(1);
     write_adc_reg(&adc.config, ADC_REG_ENTER);
@@ -31,10 +31,18 @@ int main(int argc, char *argv[]) {
                    ADC_REG_MODE_SDR | ADC_REG_MODE_TEST;
     write_adc_reg(&adc.config, ADC_REG(0, ADC_REG_MODE_ADDR, mode));
     write_adc_reg(&adc.config, ADC_REG_EXIT);
+    sleep(1);
 
+    *adc.trigger.config = ADC_TRIGGER_ONCE;
+    *adc.pack.config = channel.buffer->period_len;
+    *adc.trigger.divider = 50;
+
+    printf("Start transfer\n");
+    ioctl(channel.fd, START_XFER);
+    ioctl(channel.fd, FINISH_XFER);
     close_adc(&adc);
-
-    /*printf("Start transfer");*/
-    /*ioctl(channel.fd, START_XFER);*/
+    for (int i = 0; i < channel.buffer->period_len; i++) {
+        printf("%d\n", channel.buffer->buffer[0]);
+    }
     close_dma_channel(&channel);
 }
