0f88083347a489023ada4e25897e1b7e9002e7ed
Jonas Drotleff <jonas.drotleff@3j14.de>
2025-01-09 11:52:57 +0100

    Packetizer: Testbench update check start behaviour

--- library/adc/packetizer_tb.sv
+++ library/adc/packetizer_tb.sv
@@ -14,15 +14,19 @@ module axis_loopback_checker (
 );
     reg [31:0] data = $urandom();
     reg tvalid = 1'b1;
-    assign s_axis_tready = 1'b1;
+    reg tready = 1'b0;
+    assign s_axis_tready = tready;
     assign m_axis_tvalid = tvalid;
     assign m_axis_tdata  = data;
 
     always @(posedge aclk or negedge aresetn) begin
         if (!aresetn) begin
+            data   <= $urandom();
             tvalid <= 1;
+            tready <= 0;
         end else begin
-            if (tvalid && m_axis_tready) begin
+            tready <= 1;
+            if (m_axis_tvalid && m_axis_tready) begin
                 tvalid <= 0;
             end
             if (s_axis_tvalid && s_axis_tready) begin
@@ -40,7 +44,8 @@ module packetizer_tb #(
     localparam integer Period = $rtoi(1_000.0 / (2.0 * CLK_FREQ));
 
     bit         clk = 0;
-    bit         resetn = 0;
+    bit         resetn_loopback = 0;
+    bit         resetn_s2mm = 0;
 
     wire [31:0] s_axis_data_tdata;
     wire        s_axis_data_tvalid;
@@ -56,7 +61,7 @@ module packetizer_tb #(
 
     axis_loopback_checker loopback (
         .aclk(clk),
-        .aresetn(resetn),
+        .aresetn(resetn_loopback),
         .s_axis_tdata(m_axis_s2mm_tdata),
         .s_axis_tready(m_axis_s2mm_tready),
         .s_axis_tvalid(m_axis_s2mm_tvalid),
@@ -67,7 +72,7 @@ module packetizer_tb #(
 
     packetizer_s2mm s2mm (
         .aclk(clk),
-        .aresetn(resetn),
+        .aresetn(resetn_s2mm),
         .s_axis_data_tdata(s_axis_data_tdata),
         .s_axis_data_tvalid(s_axis_data_tvalid),
         .s_axis_data_tready(s_axis_data_tready),
@@ -85,13 +90,23 @@ module packetizer_tb #(
 
     initial begin
         #(5 * Period);
-        @(posedge clk) resetn = 1;
+        @(posedge clk) resetn_loopback = 1;
+        resetn_s2mm = 1;
         #(5 * Period);
         @(posedge clk) config_reg = 10;
         #(Period);
         @(posedge clk) if (counter != 1) $error("Counter not incremented");
         #(18 * Period);
         @(posedge clk) if (~last) $error("'last' not asserted");
+        #(40 * Period);
+        @(posedge clk) resetn_loopback = 0;
+        #(Period);
+        @(posedge clk) resetn_s2mm = 0;
+        config_reg = 0;
+        #(Period);
+        @(posedge clk) resetn_s2mm = 1;
+        #(Period);
+        @(posedge clk) config_reg = 10;
         #(40 * Period);
         $finish();
     end
