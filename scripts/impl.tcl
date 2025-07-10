83aa71b4d6b21d0ffb1183adf6b4ae0de8c0290f
Jonas Drotleff <jonas.drotleff@3j14.de>
2025-07-10 18:01:12 +0200

    Impl: Add option for number of jobs

--- scripts/impl.tcl
+++ scripts/impl.tcl
@@ -1,7 +1,8 @@
 set name [lindex $argv 0]
+set nproc [lindex $argv 1]
 open_project build/projects/$name/$name.xpr
 
 reset_run impl_1
-launch_runs impl_1 -jobs 6
+launch_runs impl_1 -jobs $nproc
 wait_on_run impl_1
 
